d141aa10162d7183ea03448815964bea23a7bc67
Heather <hruddy26@yahoo.com>
2021-07-08 20:28:18 -0400

    import libraries

---
+++ .vscode/Camping_Recipes_app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from tkinter import *
+
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import tkinter.font as tkFont
