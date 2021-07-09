f8257776bfa6a9dfa1147203e4825a9e2f28073e
Heather <hruddy26@yahoo.com>
2021-07-09 17:59:52 -0400

    print similar recipe function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 #import libraries
 import pandas as pd
 import numpy as np
@@ -5,8 +8,24 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+#import CSV file to pandas (make sure excell is in csv format)
 df = pd.read_csv ("/Users/heatherruddy/Downloads/HCI Python/Recipe Project/recipe_Project_Ruddy/recipe2.csv")
 
+
+#print 5 rows of data
+df.head(5)
+print(df)
+
+#print columns 
+col_list = df.columns
+print(col_list)
+
+#print a row
+df.loc[3]
+
+
+
+'''
 #is the recipe available
 def is_recipe_available(user_input,df):
     
@@ -18,7 +37,8 @@ def is_recipe_available(user_input,df):
         
     else:
         print("Recpie is not available")
-
+'''
+'''
 #print entire recipe 
 def print_recipe(user_input, df):
     r = df.loc[df["Recipe title"] == user_input]
@@ -28,8 +48,54 @@ def print_recipe(user_input, df):
 	    print("Not found")
     else:
 	    print(r.loc[0].tolist())
+'''
 
-#user leaves recipe review
+
+#prints directions line by line
+def print_directions(directions):
+    
+    split = 0
+    #loop through all the characters to find where we have '1.' and '2.' and '3.'
+    for i in range(len(directions) - 1): 
+        
+        if (directions[i].isnumeric()) and (directions[i+1] == '.'):
+            
+            print(directions[split:i], '\n')
+            
+            split = i  
+
+
+
+def find_similar_recipe(user_input):
+    
+    #store similar recipes to what user inputs
+    recipes = []    
+    
+    #split the user input
+    inp = user_input.lower().split()
+    
+    #make the recipe name lowercase
+    recipe_list = df['Recipe title'].tolist()
+    
+    recipe_list_adjusted = [''.join(s.split()).lower() for s in recipe_list]
+    
+    #find if the any words in the user input matches the recipe list
+    for i in inp:
+        
+        for r, recipe in enumerate(recipe_list_adjusted):
+            
+            if i in recipe:
+                
+                recipes.append(recipe_list[r])
+    #one result per title will print out           
+    return np.unique(recipes)
+
+
+
+
+
+
+'''#user leaves recipe review
 def leave_review(user_review):
     
     #if else loop for acceptable review length
@@ -37,7 +103,8 @@ def leave_review(user_review):
         print("Thank you for your review.")
     else: 
         print("Error. Your review exceeds the character limit.")
-
+'''
+'''
 #user adds a note to a recipe
 def leave_note(user_note):
     
@@ -45,35 +112,23 @@ def leave_note(user_note):
     if len(user_note) < 200:
         print("Your note has been saved.")
     else:
-        print("Eror. Your note was too long.")
+        print("Error. Your note was too long.")
+'''
 
-#print entire recipe 
-def print_recipe(user_input, df):
-    '''search title column in df for  user_input (string)
-    if found, print out full recipe, otherwise inform user that not found
-    '''
-    r = df.loc[df["Recipe title"] == user_input]
-    r.reset_index(drop=True, inplace=True)
-    print(r) # Debug
-
-    if r.empty:                 # I'd advise against printing out stuff inside a function
-	    print("Not found")      # better to return something, and have the caller catch it.
-    else:                       # It can then be up to the caller to print it out, or show it in a GUI, or whatever
-	    print(r.loc[0].tolist())# Basically it's more flexible b/c it lets the caller decide what to do
-
+'''
 #import an image for a recipe
 def plot_image(image):
     
     img = mpimg.imread(image)
     imgplot = plt.imshow(img)
-    plt.show()
+    plt.show()'''
 
-def return_best_match(search_term, word_list):
-    '''Given a search term, which is the closest match from a list of strings?
+'''def return_best_match(search_term, word_list):
+    Given a search term, which is the closest match from a list of strings?
     if not reasonably good match is found, None is returned
     Based on a difference metric, here i'm using the Jaro-Winkler metric from this package:
     https://rawgit.com/ztane/python-Levenshtein/master/docs/Levenshtein.html#Levenshtein-distance
-    '''
+    
     similarity_list = []
     for word in word_list:
         similarity = Levenshtein.jaro_winkler(search_term, word) # https://rawgit.com/ztane/python-Levenshtein/master/docs/Levenshtein.html#Levenshtein-jaro_winkler
@@ -86,31 +141,11 @@ def return_best_match(search_term, word_list):
         return None
     else:
         return best_match[1] # word with best metric (> 0.5)
+'''
+ 
 
 
 
-#Main Code 
-
-
-#print 5 rows of data
-df.head(5)
-print(df)
-#print columns 
-print(df.columns)
-
-#user input area for recipe they're searching
-user_input = input("Search Recipes \n")
-
-#print the recipe based on above user input, but
-#this does not work, I dont know what this error means
-is_recipe_available(user_input,df)
-
-
-#user input for leaving a review
-user_review = input("Leave a Review\n")
-
-#image path that I want to import
-image = "/Users/heatherruddy/Downloads/HCI Python/Recipe Project/recipe_Project_Ruddy/mushroom_burger.jpg"
 
 
 
