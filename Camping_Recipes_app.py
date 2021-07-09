49b718743bdc62aa49fd40dd97584479db7c9c2d
Heather <hruddy26@yahoo.com>
2021-07-08 21:20:34 -0400

    add window and title

---
+++ Camping_Recipes_app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from tkinter import *
+
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import tkinter.font as tkFont
+
+
+
+
+#import recipe to pandas
+root = Tk()
+root.geometry('800x500')
+root.title('Camping Recipe App')
+
+
+
+#keeps the app running
+root.mainloop()
+
+
