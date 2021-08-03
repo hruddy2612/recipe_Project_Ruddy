d878ba16348a3469413a849004f2304b567e8f31
hruddy2612 <84727990+hruddy2612@users.noreply.github.com>
2021-08-02 21:06:42 -0400

    added doc folder

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from tkinter import *
 
 import pandas as pd
 import numpy as np
-import time
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import os
 import tkinter.font as tkFont
-
 from PIL import ImageTk, Image
 
 #get current working directory
